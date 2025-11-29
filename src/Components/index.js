fda66d528accae04cb8248e0aebdb1bf980d533d
Kishan Revankar <your-email@example.com>
2025-11-29 21:48:51 +0530

    implement welcome section

---
+++ src/Components/index.js
@@ -0,0 +1,5 @@
+import Navbar from "#components/Navbar.jsx";
+import Welcome from "#components/Welcome.jsx";
+
+export { default as Navbar } from "./Navbar";
+export { default as Welcome } from "./Welcome";
